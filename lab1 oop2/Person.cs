b947c0cfe8f9f489e8351dc3f2222e0cd2fdeff2
joshc <joshc@LAPTOP-63QHI8BU>
2023-01-25 21:39:32 -0700

    Add project files.

---
+++ lab1 oop2/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_oop2
+{
+    internal class Person
+    {
+        public int personId;
+        public string firstName;
+        public string lastName;
+        public string favoriteColour;
+        public int age;
+        public bool isWorking;
+
+
+        public string DisplayPersonInfo()
+        {
+             return " \n Person ID: " + personId + "\n First Name: " + firstName + " \n last Name: " + lastName + " \n Favorite Color: " + favoriteColour + " \n Age: " + age + " \n isworking: " + isWorking;
+
+        }
+
+
+        public void ChangeFavoriteColor()
+        {
+            favoriteColour = "white";
+        }
+
+
+        public void GetAgeInTenyears()
+        {
+            age += 10;
+        }
+
+
+        public string ToString()
+        {
+            return  personId + ":" + firstName + " " + lastName + " " + "Favorite color is" + " " + favoriteColour;      
+        }
+
+
+
+        public Person(int Id, string firstName , string LastName, string favoriteColor, int age, bool isWorking)
+        {
+            this.personId = Id;
+            this.firstName = firstName;
+            this.lastName = LastName;
+            this.favoriteColour = favoriteColor;
+            this.age = age;
+            this.isWorking = true; 
+        }
+
+       
+            
+
+
+        
+    }
+
+
+}
---
+++ lab1 oop2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_oop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1;
+
+            person1 = new Person(1, "Ian", "Brooks","Red",30, true);
+
+            Person person2;
+
+            person2 = new Person(2, "Gina", "James", "Green", 18, false);
+
+            Person person3;
+
+            person3 = new Person(3,"Mike", "Briscoe","Blue", 45, true);
+
+            Person person4;
+
+            person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            Console.WriteLine(person2.ToString());
+
+            Console.WriteLine(person3.DisplayPersonInfo());
+
+            person1.ChangeFavoriteColor();
+            Console.WriteLine(person1.ToString());
+
+            person4.GetAgeInTenyears();
+            Console.WriteLine(person4.firstName + " " + person4.lastName + "'s age in 10 years is" +  " " + person4.age );
+
+           
+
+
+
+
+        }
+    }
+}
